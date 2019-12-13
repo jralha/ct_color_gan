d148ff5e66d4329217f388df7e91ed52eac543ae
jralha <42118093+jralha@users.noreply.github.com>
2019-12-13 10:31:36 -0300

    batch fix BGs, attemp to test model on nii files

--- bg_fix.py
+++ bg_fix.py
@@ -2,36 +2,45 @@
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import argparse
+import glob
+from tqdm.auto import tqdm
+import os
 
 #%%
+parser = argparse.ArgumentParser()
+parser.add_argument('--name',required=True)
+parser.add_argument('--run',required=True)
+args = parser.parse_args()
 
+name = args.name
+run = args.run
 
-real = cv2.imread('ct_color_gan\\results\\ct_color\\test_latest\\images\\88_real.png')
-fake = cv2.imread('ct_color_gan\\results\\ct_color\\test_latest\\images\\88_fake.png')
+img_folder = 'results'+'\\'+name+'\\'+run+'\\'+'images'
+out_folder = 'results'+'\\'+name+'\\'+run+'\\'+'bg_fix'
 
-# real = cv2.cvtColor(real,cv2.COLOR_BGR2RGB)
-# fake = cv2.cvtColor(fake,cv2.COLOR_BGR2RGB)
+if not os.path.exists(out_folder):
+    os.mkdir(out_folder)
 
-real_bw = cv2.cvtColor(real,cv2.COLOR_RGB2GRAY)
+img_list = glob.glob(img_folder+'\\'+'*.png')
 
-# bg_bin = cv2.threshold(real_bw,0,255,cv2.THRESH_OTSU)[1]
-bg_bin = cv2.threshold(real_bw,0,255,cv2.THRESH_BINARY)[1]
+i = 0
+for img in tqdm(img_list):
+    if 'real' in img:
+        real = cv2.imread(img)
+        fake = cv2.imread(img_list[i-1])
 
+        real_bw = cv2.cvtColor(real,cv2.COLOR_RGB2GRAY)
 
-bg_bin = cv2.cvtColor(bg_bin,cv2.COLOR_GRAY2RGB)//255
-test = bg_bin*fake
+        bg_bin = cv2.threshold(real_bw,0,255,cv2.THRESH_BINARY)[1]
+        bg_bin = cv2.cvtColor(bg_bin,cv2.COLOR_GRAY2RGB)//255
 
-plt.figure(figsize=[100,30])
-plt.subplot(4,1,1)
-plt.imshow(fake)
-plt.subplot(4,1,2)
-plt.imshow(real)
-plt.subplot(4,1,3)
-plt.imshow(bg_bin)
-plt.subplot(4,1,4)
-plt.imshow(test[:,:,[2,1,0]])
-# %%
-cv2.imwrite('test.png',test)
+        bg_fix = bg_bin*fake
+
+        outname = img.split('\\')[-1].split('_')[0]
+        out = out_folder+'\\'+outname+'.png'
+
+        cv2.imwrite(out,bg_fix)
+    i=i+1
 
 # %%
---
+++ test_on_nii.py
@@ -0,0 +1,56 @@
+#%%
+import os
+import sys
+
+cdir = os.getcwd()
+os.chdir('ct_color_gan')
+from models.base_model import BaseModel
+from models.test_model import TestModel
+from models import networks, create_model
+from options.test_options import TestOptions
+os.chdir(cdir)
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import nibabel as nib
+
+#%%
+nii = np.expand_dims(np.expand_dims(np.uint8(np.array(nib.load('ct_color_gan\\nii_data\\Libra_CX1_T1_221_184_1485_Energy1.nii' ).get_data())),axis=0),axis=0)[:,:,:-1,:,:-1]
+nii = torch.tensor(nii)
+
+checkpoint = 'ct_color_gan\\checkpoints\\ct_color\\latest_net_G.pth'
+
+#%%
+netG = networks.define_G(input_nc=3,output_nc=3,ngf=64,norm='instance',netG='resnet_9blocks',gpu_ids=[])
+netG.load_state_dict(torch.load(checkpoint))
+
+# %%
+def conv2dto3d(module):
+        kernel_size = module.kernel_size[0]
+        stride = module.stride[0]
+        padding = module.padding[0]
+        weight = module.weight.unsqueeze(2) / kernel_size
+        weight = torch.cat([weight for _ in range(0, kernel_size)], dim=2)
+        bias = module.bias
+
+        module = nn.Conv3d(in_channels=module.weight.shape[1], out_channels=module.weight.shape[0],
+                               kernel_size=kernel_size, padding=padding, stride=stride, bias=True)
+        module.bias = bias
+        return module
+
+modules = {}
+for name, module in netG.named_modules():
+    if(isinstance(module, nn.Conv2d)):
+        module = conv2dto3d(module)
+
+       
+    if 'conv_block' in name:
+        for name2,module2 in module.named_modules():
+            if(isinstance(module2, nn.Conv2d)):
+                module2 = conv2dto3d(module)
+
+                print(module2) 
+        
+# %%
