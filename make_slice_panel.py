00512022fa2318a122e7078ecd03a8b613dc7b71
jralha <42118093+jralha@users.noreply.github.com>
2020-02-18 17:29:11 -0300

    make panel

---
+++ make_slice_panel.py
@@ -0,0 +1,48 @@
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import argparse
+
+plt.style.use('dark_background')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--niifile',required=True)
+# parser.add_argument('--method',required=True)
+parser.add_argument('--dim',required=True)
+parser.add_argument('--slices',required=True)
+parser.add_argument('--c_map',default='gray')
+args = parser.parse_args()
+
+filename = args.niifile
+# method = args.method
+dim = args.dim
+slcs = args.slices.split(',')
+c_map = args.c_map
+
+niiarray = np.array(nib.load(filename).get_data())
+
+if dim == 'xy':
+    array= niiarray.T
+elif dim == 'xz':
+    array=niiarray
+elif dim == 'yz':
+    array=np.swapaxes(niiarray,1,2)
+
+nimg=len(slcs)
+nx=np.ceil(np.sqrt(len(slcs)))
+ny=nx
+cimg=1
+plt.figure()
+for s in slcs:
+    plt.subplot(nx,ny,cimg)
+    plt.imshow(array[int(s)],cmap=c_map)
+    plt.yticks([])
+    plt.xticks([])
+    # for spine in plt.gca().spines.values():
+    #     spine.set_visible(False)
+    # plt.title('Slice '+str(s))
+    cimg=cimg+1
+
+plt.tight_layout()
+plt.show()
